4a19c759e9a51d19328200e9f4fe6dca68612c9a
YOONSU PARK <shinebytheriver@yahoo.com>
2017-11-30 15:27:30 -0800

    Add files via upload

--- underscoreMagic_.scala
+++ underscoreMagic_.scala
@@ -88,15 +88,84 @@ object underscoreMagic_ {
       case _ => "more than two"
     }
 
-    def TestType[T](x:T):String = x match {  //output type :String can be omitted
+//    def TestType[T](x:T):String = x match {  //output type :String can be omitted
+    def TestType[Any](x:Any) = x match {  //output type :String can be omitted
       case _: String => "String"
       case _: Double => "Double"
       case _  => "None"
     }
+    def myWorkPlan(weekday:Int) = weekday match {
+      case 1 | 2 | 3 | 4| 5 => "work"
+      case 6 | 7 => "no work"
+    }
+
+    def parseArgument(arg: String) = arg match {
+      case "-h" | "--help" => displayHelp
+      case "-v" | "--version" => displayVersion
+      case whatever => unKnownArgment(whatever)
+    }
+
+    def displayHelp = "Help"
+    def displayVersion = "Version"
+//    def unKnownArgment(str: String) = displayHelp _
+    def unKnownArgment(str: String) = println(str)
+
 
     val testProperties2 = TestPattenMatch(3)
     println("TestPattenMatch(3) :" +testProperties2)
 
+    println("TestType : "+TestType("this"))
+
+    println("myWorkPlan : "+myWorkPlan(7))
+
+    println("*** parseArgument : "+ parseArgument("pp"))
+    parseArgument("pp")
+    println("--- parseArgument : "+ parseArgument("-h"))
+    val xs = (1 to 10 by 2 ).toList
+    println("xs: "+xs)
+    println("1 :"+List(1 to 10))  //Wrong expression ! : output List(Range 1 to 10)
+    println((1 to 10).toList)
+    println("2 :"+List(1,2,3,4,5,6,7,8,9,10))
+
+    val ys = List(1,2,3,4,5,6,7,8,9,10).filter(_%2 == 0)
+    val ys2 = (1 to 10).toList.filter(x=> x%2 ==0)  //same as above
+    println("ys :" +ys)
+    println("ys2 :" +ys2)
+/* zip makes a sequence of tuples */
+    /* destructuring bind by zip of lists*/
+    println("** for ((x,y) <- xs zip ys) yield x*y :" + (for ((x,y) <- xs zip ys) yield x*y ) )
+    println( "*(xs zip ys) map { case (x,y) => x*y }:" +( (xs zip ys) map { case (x,y) => x*y } ))//same as above
+    //    (xs zip ys) map( (x,y) => x*y )//error : type mismatch !
+    val seqOfTuples = xs zip ys
+    println("tuple made from zip : ")
+    seqOfTuples.foreach(x => print(x + " "));println()
+    println("@ tuple to map : ")
+    println(seqOfTuples.toMap)
+
+    val products = Array("breadsticks", "pizza", "soft drink")
+    val prices = Array(4)
+    val productsWithPrice = products.zip(prices)
+    println("products.zip(prices) : "+products.zip(prices))
+    println()
+/*  unzip: convert a zipped tuple to each original List */
+    println(List((1,"a"), (3, "b"), (4, "d")).unzip)
+    println(List((1,"a", true), (3, "b", false), (4, "d", true)).unzip3)
+
+    println(List("a", "b", "c").zipWithIndex) //List((a,0), (b,1), (c,2))
+    println(List("a", "b", "c").zipWithIndex.unzip)  //(List(a, b, c),List(0, 1, 2))
+    var (ids, indexes) =
+      List("a", "b", "c").zipWithIndex.unzip
+    println((ids, indexes))  //(List(a, b, c),List(0, 1, 2))
+
+    /* cross product by each list*/
+    println("cross product :")
+    println("* for (x <- xs; y <- ys) yield x*y : "+ (for (x <- xs; y <- ys) yield x*y))
+    println("* xs flatMap {x => ys map {y => x*y} : "+(xs flatMap {x => ys map {y => x*y}}))
+
+    /* for(x <-xs; y <-ys; condition) yield zs
+      * xs.flatMap(x => for(y <- ys; condition) yield zs)  */
+    println("* 5 :" + ( for(x <- xs; y <- ys ; if x*x > y*y ) yield x*y ) )
+    println("* 6 : "+  xs.flatMap(x => (for(y <- ys; if x*x > y*y )yield x*y ) ) )
 
   }//end of main
 
